11506292d68ffa3eef3b0c66dcc2880df169ac23
Alhai <aly.haidara@seira-formation.fr>
2024-05-16 16:08:41 +0200

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import './App.css'
+import "./App.css";
 
-import Counter from './Counter/Counter'
-import HelloWorld from './HelloWorld/Helloworld'
-import List from './List/List'
-import NameForm from './NameForm/NameForm'
-import { useState } from 'react'
+import Counter from "./Counter/Counter";
+import HelloWorld from "./HelloWorld/Helloworld";
+import List from "./List/List";
+import NameForm from "./NameForm/NameForm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <HelloWorld></HelloWorld>
       <Counter></Counter>
-    <List></List>
-    <NameForm></NameForm>
+      <List></List>
+      <NameForm></NameForm>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/List/List.tsx
+++ src/List/List.tsx
@@ -1,28 +1,17 @@
 import "./List.css";
 
-import { useState } from "react";
-
 const ArrayList = ["Thomas", "Zarah", "Antoine", "Yohann", "Jeremy"];
 
 export default function List() {
-  const [value, setValue] = useState("");
 
   return (
-    // <div>
-    //   {List.map((prenom) => (
-    //     <li>{prenom}</li>
-    //   ))}
-    // </div>
+
     <div>
       <div>
         {ArrayList.map((prenom, i) => (
           <li
             key={i}
-            className={i % 2 === 0 ? "Navy" : "rien"}
-            // style={{
-            //   color: i % 2 === 0 ? "blue" : "red",
-            // }}
-          >
+            className={i % 2 === 0 ? "Navy" : "rien"}>
             {prenom}
           </li>
         ))}
